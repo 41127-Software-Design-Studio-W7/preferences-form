768d577aac0a26322948e1c17e31550c2e25e64b
Alex Trobec <alex.trobec@immutable.com>
2022-10-14 20:19:51 +1100

    usestate for search term working

--- src/Components/SearchField.js
+++ src/Components/SearchField.js
@@ -3,14 +3,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export default function SearchField() {
+
     const [searchTerm, setsearchTerm] = useState("");
+
+    const onChange = (e) => {
+        setsearchTerm(e.target.value);
+        console.log(searchTerm);
+    }
 
     return (
         <div>
             <div>
                 Search:
             </div>
-            <input type="text" value={searchTerm} placeholder="Enter search term here">
+            <input type="text" value={searchTerm} onChange={onChange} placeholder="Enter search term here">
 
             </input>
         </div>
